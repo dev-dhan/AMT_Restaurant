9fd8110c0d8b7ea84507aa7d9bf6acbea1c47461
dev-dhan <roldhan.fulo.alcantara@gmail.com>
2023-02-02 23:56:08 +1100

    This is initial Commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+hamburger = document.querySelector(".hamburger" );
+hamburger.onclick = function() { 
+    navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
+}
+
+
+ 
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
